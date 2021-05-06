178a88e4ff6e66d777c645f467717aa408193bba
Lorenz Seufert <lorenzseufert@gmail.com>
2021-05-06 22:14:30 +0200

    Move Method getFrequentRenterPoints

--- src/main/java/de/dhbw/se/refactoring/Rental.java
+++ src/main/java/de/dhbw/se/refactoring/Rental.java
@@ -23,10 +23,6 @@ class Rental {
     }
 
     public int getFrequentRenterPoints() {
-        if ((this.getMovie().getPriceCode() == Movie.NEW_RELEASE) && this.getDaysRented() > 1) {
-            return 2;
-        } else {
-            return 1;
-        }
+        return this.movie.getFrequentRenterPoints(this.daysRented);
     }
 }
