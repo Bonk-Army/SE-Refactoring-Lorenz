7e0f7fc9c604d5e46a074c265ee7c07074ba96fd
Lorenz Seufert <lorenzseufert@gmail.com>
2021-05-06 19:51:21 +0200

    Added TestSuite to run all tests at once

---
+++ src/test/java/de/dhbw/se/refactoring/TestSuite.java
@@ -0,0 +1,9 @@
+package de.dhbw.se.refactoring;
+
+import org.junit.platform.runner.JUnitPlatform;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.runner.RunWith;
+
+@RunWith(JUnitPlatform.class)
+@SelectPackages("de.dhbw.se.refactoring")
+public class TestSuite {}
